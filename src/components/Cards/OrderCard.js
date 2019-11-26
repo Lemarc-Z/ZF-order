76fe64f6648984ec4bfd4898911f8e7fbc5ff541
EthanXY <ethan--z@hotmail.com>
2019-11-26 15:08:14 +0800

    - add dialog

--- src/components/Cards/OrderCard.js
+++ src/components/Cards/OrderCard.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 //
 import UniTextField from "../Universal/UniTextField";
 import UniSelect from "../Universal/UniSelect";
+
 // import LingoSelect 			from '../Special/LingoSelect';
 // helpers;
 import HttpHelper from "../Helpers/HttpHelper";
@@ -19,12 +25,18 @@ function SignupCard(props) {
   const [model, setModel] = useState("");
   const [type, setType] = useState("");
   const [color, setColor] = useState("");
-  const [pieces, setPieces] = useState("");
-  const [price, setPrice] = useState("");
-  const [amount, setAmount] = useState("");
+  const [pieces, setPieces] = useState(0);
+  const [price, setPrice] = useState(0);
+
+  const [open, setOpen] = useState(false);
+  const [confirmation, setConfirmation] = useState([]);
 
   const colors = ["浅枪", "亮银", "白钢"];
   const types = ["镜架", "脚丝", "鼻中"];
+
+  function handleClose() {
+    setOpen(false);
+  }
 
   // const content = LingoContentHelper.contentByLang();
 
@@ -43,6 +55,21 @@ function SignupCard(props) {
       if (ValidateHelper.validateUserArgs(userArgs))
         throw ValidateHelper.validateUserArgs(userArgs);
 
+      let amount = price * pieces;
+
+      let confirmation0 = [
+        { name: "厂家", val: customer },
+        { name: "型号", val: model },
+        { name: "类型", val: type },
+        { name: "镀色", val: color },
+        { name: "数量", val: pieces },
+        { name: "单价", val: price },
+        { name: "价格", val: amount }
+      ];
+      console.log(`- confirmation: ${JSON.stringify(confirmation)}`);
+      setOpen(true);
+      setConfirmation(confirmation0);
+      // console.log(`- selectedValue: ${JSON.stringify(selectedvalue)}`);
       //   const postUrl = 'https://serko-engineering-exercises.azurewebsites.net/api/SignUp';
       //   const resobj = await HttpHelper.httpRequestA(postUrl, { email, password, preferredLanguage: language }, 1);
       //   console.log(`- resobj: ${JSON.stringify(resobj)}`);
@@ -98,6 +125,32 @@ function SignupCard(props) {
       >
         <b>提 交</b>
       </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <List>
+            {confirmation.map(item => (
+              <ListItemText
+                primary={item.name}
+                secondary={item.val}
+                className={classes.inline}
+              />
+            ))}
+          </List>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            确 认
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            取 消
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Card>
   );
 }
@@ -117,6 +170,11 @@ var useStyles = makeStyles({
   button: {
     marginTop: "20px",
     width: "80%"
+  },
+  inline: {
+    display: "flex",
+    flexDirection: "row",
+    padding: "10px 20px"
   }
 });
 
