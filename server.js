ce58d4ee37c8bf3898a95a704497adbf79c3200c
Lemarc-Z <lemarc.zac@gmail.com>
2019-12-17 19:12:56 +0800

    - fx

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ var Koa = require("koa");
 var compress = require("koa-compress");
 var helmet = require("koa-helmet");
 var logger = require("koa-logger");
-
+var send = require ('koa-send');
 var app = new Koa();
+
 app.use(helmet());
 app.use(compress());
 
 app.use(logger());
 
-// Serve the static files from the build folder
-app.use(express.static(__dirname + "/build"));
-//app.use('/material-dashboard-react', express.static(__dirname + "/build"));
-// Redirect all traffic to the index
-app.get("*", function(req, res) {
-  res.sendFile(__dirname + "/build/index.html");
-});
+app.use(async (ctx) => {
+  let sendOpts = {
+    root: 'build',
+    maxage: 7 * 24 * 3600 * 1000,
+  };
+  await send (ctx, '/index.html', sendOpts);
+})
+
 // Listen to port 3000
 app.listen(process.env.PORT || 3000);
